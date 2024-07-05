42947f6c38a9f08dff7ca1585ff3a12a09a33e2c
Biswajit Biswas <bshwjt@gmail.com>
2024-07-05 20:11:29 +0530

    Added scipts

---
+++ Scripts/19.ps1
@@ -0,0 +1,21 @@
+﻿$domain = (Get-ADDomain).DistinguishedName 
+
+$DCs = Get-ADGroupMember -Identity "Domain Controllers" | Select-Object -ExpandProperty Name 
+
+$Objs = Foreach ($DC in $DCs){
+    Get-ADObject -Filter {Name -eq "SYSVOL Subscription"} -SearchBase "CN=Domain System Volume,CN=DFSR-LocalSettings,CN=$DC,OU=Domain Controllers,$domain" -Properties DistinguishedName, msDFSR-Enabled, msDFSR-options | 
+    Select-Object DistinguishedName, msDFSR-Enabled, msDFSR-options
+}
+
+foreach ($Obj in $Objs){
+    $msDFSR_options = $Obj.'msDFSR-options'
+    if ([string]::IsNullOrWhiteSpace($msDFSR_options)) {
+        $msDFSR_options = "<not set>"
+    }
+
+    [PSCustomObject]@{ 
+        DomainController = ($($Obj.DistinguishedName) -split ",")[3].Substring(3)
+        "msDFSR-Enabled" = $($Obj.'msDFSR-Enabled')
+        "msDFSR-options" = $msDFSR_options
+    } 
+}
